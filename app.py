daa63239807732005e1ea2db4c9a7a34accc5e03
Enzo Lizama <lizama.enzo@gmail.com>
2020-06-28 22:39:30 -0500

    build up server

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import json
+import time
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:8080"}})
@@ -18,7 +19,29 @@ def hello():
 @app.route('/sentiment-analysis', methods=['POST'])
 @cross_origin()
 def predict():
-    return {"data": "Gmi2 puto"}
+    post_data = request.data.decode("utf-8")
+    data = json.loads(post_data)
+    time.sleep(5)
+    # number_tweets
+    return {
+        "mean_text":
+        "Negativo",
+        "mean":
+        -1,
+        "details": [{
+            "text": "Gmi2 puto",
+            "accuracy": 52.1,
+            "result": "negativo",
+        }, {
+            "text": "Andrecito cagon",
+            "accuracy": 82.1,
+            "result": "negativo"
+        }, {
+            "text": "Miguleito xd",
+            "accuracy": 92.1,
+            "result": "neutro"
+        }]
+    }
 
 
 if __name__ == "__main__":
